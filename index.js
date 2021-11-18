7523b67d6309cb7630741166d57851fcd901ff24
RodxDesign <rodrigopralv@gmail.com>
2021-11-17 18:40:18 -0600

    Boton-nav

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const btn = document.querySelector(".nav-toggle");
+
+const navBar = document.querySelector(".nav-menu");
+
+const img = document.querySelectorAll(".container img");
+
+
+
+
+
+
+
+
+
+
+// addEventListener´s
+
+btn.addEventListener("click", () => {
+   navBar.classList.toggle("nav-menu_visible");
+})
+
